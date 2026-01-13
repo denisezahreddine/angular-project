a02e967d6115ae2251e02fcdfa13cf358a831c54
Olfa Laarif <olfa.laarif@gmail.com>
2026-01-13 13:57:09 +0100

    correct api

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://coding-bank.fly.dev/api',
+  token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMzU1OTgyNSJ9.vQ4g3OzYmLEDhxwQVYcyhOI6EdFOYhcfQSXh0kZ0nOw',
+};
