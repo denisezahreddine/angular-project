77b152b56b7be78bfd62bfc26d75b16eb05a94c5
wassila20 <wassila.belkhamsa@edu.esiee-it.fr>
2026-01-13 14:06:14 +0100

    shred component

---
+++ src/app/shared/lien-component/lien-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lien-component',
+  imports: [],
+  templateUrl: './lien-component.html',
+  styleUrl: './lien-component.css',
+})
+export class LienComponent {
+
+}
