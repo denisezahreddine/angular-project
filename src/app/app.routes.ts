beb26466f525b4409015fea16b42e3ff1c8533d7
wassila20 <wassila.belkhamsa@edu.esiee-it.fr>
2026-01-14 14:18:25 +0100

    fix after merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,39 +2,33 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import {authGuard} from './core/auth.guard';
+import { LayoutComponent } from './shared/layout/layout';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    //path: 'comptes',
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: '**', // Redirige les erreurs 404 vers login
-    redirectTo: 'login'
-    },
+
+ {
     path: '',
     component: LayoutComponent, // On enveloppe tout dans le layout
     children: [
       {
-        path: 'comptes',
-        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [authGuard]
       },
       {
         path: '',
-        redirectTo: 'comptes',
+        redirectTo: 'home',
         pathMatch: 'full'
-      }
+      },
+
     ]
-  }
+  },
+  {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '**', // Redirige les erreurs 404 vers login
+        redirectTo: 'login'
+      },
 ];
