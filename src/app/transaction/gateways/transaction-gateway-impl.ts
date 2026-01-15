dd9d9e26ffab7c839ae3bdc24ef94e1826565d1f
denise zahreddine <denise.zahreddine@gmail.com>
2026-01-16 00:08:10 +0100

    fix after merge

--- src/app/transaction/gateways/transaction-gateway-impl.ts
+++ src/app/transaction/gateways/transaction-gateway-impl.ts
@@ -3,13 +3,14 @@ import {Observable, tap} from 'rxjs';
 
 import {TransactionGateway} from './transaction-gateway';
 import {TransactionResponse, TransactionsApi} from '../../compte/api/transactions.api';
+import {Transaction} from '../../compte/models/transaction.model';
 
 @Injectable({providedIn: 'root'})
 export class TransactionGatewayImpl implements TransactionGateway {
 
   private api = inject(TransactionsApi);
 
-  cancelTransaction(transactionId: string): Observable<TransactionResponse[]> {
+  cancelTransaction(transactionId: string): Observable<Transaction> {
 
     return this.api.cancelTransaction(transactionId).pipe(
       tap(response => {
--- src/app/transaction/gateways/transaction-gateway.ts
+++ src/app/transaction/gateways/transaction-gateway.ts
@@ -1,7 +1,8 @@
 import {Observable} from 'rxjs';
 import {LoginResponse} from '../../compte/models/loginResponse.model';
 import {TransactionResponse, TransactionsApi} from '../../compte/api/transactions.api';
+import {Transaction} from '../../compte/models/transaction.model';
 
 export abstract class TransactionGateway {
-  abstract cancelTransaction(transactionId: string) : Observable<TransactionResponse[]>;
+  abstract cancelTransaction(transactionId: string) : Observable<Transaction>;
 }
