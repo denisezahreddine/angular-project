07003ba5d5996df6ca55de164e382009b782a1dc
denise zahreddine <denise.zahreddine@gmail.com>
2026-01-15 18:42:17 +0100

    working on refa

--- src/app/registration/usecase/register-usecase.service.ts
+++ src/app/registration/usecase/register-usecase.service.ts
@@ -1,11 +1,13 @@
 //applique les regles de login ( 8 chiffre/6 chiffre) et appel auth.service
 import {Injectable, inject} from '@angular/core';
 import {AuthGateway} from '../gateway/auth-gateway';
-import {Observable, take} from 'rxjs';
+import {Observable, take, tap} from 'rxjs';
 import {Router} from '@angular/router';
 import {AuthStore} from '../../compte/store/auth.store';
 import {EventBus} from '../../compte/event/EventBus';
-import {RegistrationSuccessEvent} from '../../compte/event/RegisterListener';
+import {RegistrationEvent} from '../../compte/event/RegistrationEvent';
+import {ErrorData} from '../../compte/event/errorData';
+
 
 
 @Injectable({providedIn: 'root'})
@@ -16,18 +18,17 @@ export class RegisterUsecase {
 
   execute(clientCode: string, password: string) {
     return this.gateway.register(clientCode, password)
-      .pipe(take(1))
-      .subscribe({
-        next: (res) => {
-          // L'API a déjà fait le localStorage.setItem dans son 'tap'
-          // Maintenant on prévient le STORE que c'est un succès
-          this.store.setLoginSuccess(res.user.name,res.user.clientCode);
-          this.eventBus.emit(new RegistrationSuccessEvent(res.user.clientCode));;
-        },
-        error: (err) => {
-          this.store.setError(err.message);
-        }
-      });
+      .pipe(take(1),
+      tap({
+      next: (res) => {
+        this.store.setLoginSuccess(res.user.name, res.user.clientCode);
+        this.eventBus.emit(new RegistrationEvent(RegistrationEvent.success, {clientId: res.user.clientCode}));
+      },
+      error: (err) => {
+        this.store.setError(err.message);
+        this.eventBus.emit(new RegistrationEvent(RegistrationEvent.error, {message: err.message}));
+      }
+    }));
 
 
   }
